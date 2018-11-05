975c8e74d8ab6a2a5a02e996a18f4f01e6fd2494
dat <datdta95@gmail.com>
2018-11-05 11:58:29 +0700

    ..

---
+++ src/main/java/com/thinkpad/controller/CustomerController.java
@@ -0,0 +1,43 @@
+package com.thinkpad.controller;
+
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
+import com.thinkpad.model.Customer;
+import com.thinkpad.service.CustomerService;
+import com.thinkpad.service.CustomerServiceImpl;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class CustomerController {
+    private CustomerService customerService = new CustomerServiceImpl();
+
+    @GetMapping("/")
+    public ModelAndView home() {
+        return new ModelAndView("index", "customers", customerService.findAll());
+    }
+
+    @GetMapping("/customer/create")
+    public ModelAndView create() {
+        return new ModelAndView("create", "customer", new Customer());
+    }
+
+    @PostMapping("/customer/save")
+    public ModelAndView save(Customer customer) {
+        customer.setId((int) (Math.random() * 10000));
+        customerService.save(customer);
+        ModelAndView modelAndView = new ModelAndView("index", "customers", customerService.findAll());
+        modelAndView.addObject("success", "Save");
+//        redirectAttributes.addFlashAttribute("success","Saved");
+        return modelAndView;
+    }
+
+//    public String save(Customer customer, RedirectAttributes redirect) {
+//        customer.setId((int)(Math.random() * 10000));
+//        customerService.save(customer);
+//        redirect.addFlashAttribute("success", "Saved customer successfully!");
+//        return "redirect:/";
+//    }
+}
